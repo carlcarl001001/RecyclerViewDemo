09e5977d269026c2527073ad07601777608ef450
carlcarl001001 <carlcarl001@126.com>
2020-09-16 20:05:54 +0800

    first commit

---
+++ app/src/main/java/com/example/carl/recyclerviewdemo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.carl.recyclerviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv = findViewById(R.id.rv2);
+
+        rv.setLayoutManager(new GridLayoutManager(this,5));
+
+        rv.setAdapter(new GridAdapter(this));
+    }
+}
